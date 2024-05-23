2429825bcfc2ac97ade5a6bb1d0c30a869672c1c
g3m3n30 <antkey.ko@gmail.com>
2024-05-24 00:54:37 +0700

    Create setup_database.py

---
+++ streamlit/setup_database.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def setup_database():
+    conn = sqlite3.connect('binance_p2p.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS p2p_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp TEXT,
+            price REAL,
+            limit REAL,
+            buysell TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
